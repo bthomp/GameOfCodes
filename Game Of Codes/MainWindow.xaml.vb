21d6b0de1f5399e2228d5d311df59580f5c4372f
bthomp <bthomp14.bt@gmail.com>
2015-01-18 20:25:49 -0500

    Added 4 buttons, each with events that say what the user did

--- Game Of Codes/MainWindow.xaml.vb
+++ Game Of Codes/MainWindow.xaml.vb
@@ -1,3 +1,18 @@
 ﻿Class MainWindow 
 
+    Private Sub btnFirst_Click(sender As Object, e As RoutedEventArgs)
+        firstTB.Text = "Clicked first button"
+    End Sub
+
+    Private Sub btnSecond_Click(sender As Object, e As RoutedEventArgs)
+        secondTB.Text = "Clicked second button"
+    End Sub
+
+    Private Sub btnThird_Click(sender As Object, e As RoutedEventArgs)
+        thirdTB.Text = "Clicked third button"
+    End Sub
+
+    Private Sub btnFourth_Click(sender As Object, e As RoutedEventArgs)
+        fourthTB.Text = "Clicked fourth button"
+    End Sub
 End Class
