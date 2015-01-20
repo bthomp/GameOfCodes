2e43b19239871558d764b19214d4de494db46c2b
bthomp <bthomp14.bt@gmail.com>
2015-01-19 21:08:16 -0500

    trying to get a red circle image to follow the cursor

--- Game Of Codes/MainWindow.xaml.vb
+++ Game Of Codes/MainWindow.xaml.vb
@@ -9,7 +9,7 @@
     End Sub
 
     Private Sub btnSecond_Click(sender As Object, e As RoutedEventArgs)
-        secondTB.Text = "Shit"
+        secondTB.Text = "Stuff"
         firstTB.Text = ""
         thirdTB.Text = ""
         fourthTB.Text = ""
@@ -30,8 +30,27 @@
         secondTB.Text = ""
         thirdTB.Text = ""
         bottomRowBorder.Background = Brushes.PaleGoldenrod
+    End Sub
 
-
-
+    Private Sub FindMouse(sender As Object, e As MouseEventArgs) Handles MainCanvas.PreviewMouseMove
+        Dim position As Point = e.GetPosition(MainCanvas)
+        Dim circle As New Image
+        Dim bm As New BitmapImage
+        bm.BeginInit()
+        bm.UriSource = New Uri("C:\MyDev\Game Of Codes\Game Of Codes\Resources\Images\red circle.png", UriKind.Absolute)
+        bm.EndInit()
+        circle.Source = bm
+        circle.Height = circle.Source.Height
+        circle.Width = circle.Source.Width
+        circle.Stretch = Stretch.None
+        circle.UpdateLayout()
+        MainCanvas.Children.Add(circle)
+        Dim location As New Rect
+        location.Location = position
+        location.Height = circle.Height
+        location.Width = circle.Width
+        MainCanvas.Children.Item(0).Arrange(location)
+        InvalidateArrange()
+        MainCanvas.UpdateLayout()
     End Sub
 End Class
